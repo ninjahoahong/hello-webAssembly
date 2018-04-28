bd7761b9e02c28a2d1a8aef312abde76c8dcbb11
Anh Duong <ninjahoahong@users.noreply.github.com>
2018-04-28 23:23:17 +0300

    Add some test with make file

--- app/gl/src/main.c
+++ app/gl/src/main.c
@@ -20,12 +20,21 @@ int main(void)
         return -1;
     }
 
+    glfwDefaultWindowHints();
+
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+
+    // Tell GL to only draw onto a pixel if the shape is closer to the viewer
+    glEnable(GL_DEPTH_TEST); // enable depth-testing
+    glDepthFunc(GL_LESS); // depth-testing interprets a smaller value as "closer"
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
+        // wipe the drawing surface clear
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
